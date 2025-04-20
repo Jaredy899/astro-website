0f99421abb9d3206f4ab363a38131fc0701a5ccc
Jared Cervantes <jared@jaredcervantes.com>
2025-04-19 21:54:58 -0400

    redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,23 @@ export default defineConfig({
   output: 'static',
   build: {
     assets: '_assets'
+  },
+  redirects: {
+    '/debian': {
+      status: 301,
+      destination: 'https://raw.githubusercontent.com/Jaredy899/linux/refs/heads/main/config_changes/preseed.cfg'
+    },
+    '/linux': {
+      status: 301,
+      destination: 'https://raw.githubusercontent.com/Jaredy899/linux/refs/heads/main/linux.sh'
+    },
+    '/win': {
+      status: 301,
+      destination: 'https://raw.githubusercontent.com/Jaredy899/win/main/first-setup.ps1'
+    },
+    '/mac': {
+      status: 301,
+      destination: 'https://raw.githubusercontent.com/Jaredy899/mac/main/setup.sh'
+    }
   }
 });
