88bade22a079644677a48a5039160d9029092325
Jared Cervantes <jared@jaredcervantes.com>
2025-05-24 12:42:51 -0400

    many changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
+
   build: {
     assets: '_assets'
   },
+
   markdown: {
     shikiConfig: {
       theme: 'github-dark'
     }
-  }
+  },
+
+  integrations: [mdx()]
 });
